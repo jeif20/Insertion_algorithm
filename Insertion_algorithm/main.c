23046d8c8e1b24525326ca2ae187a8b3792bd190
Juan Andonaire <andonaire.juan@gmail.com>
2019-01-19 22:24:51 -0500

    Initial Commit

---
+++ Insertion_algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Insertion_algorithm
+//
+//  Created by Juan Andonaire on 1/19/19.
+//  Copyright © 2019 Juan Andonaire. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
